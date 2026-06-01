c9388a30ba91f28c9788168e73c0974a7b682eb5
Jena <pitabash811@gmail.com>
2026-06-01 12:56:11 +0530

    fixed app creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ Base.metadata.create_all(bind=engine)
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
