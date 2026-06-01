4046b6f87c0f160d13173918a241a373c2b1d3bc
Jena <pitabash811@gmail.com>
2026-06-01 13:13:05 +0530

    cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,14 +10,17 @@ from app.models import Product, Customer, Order
 
 Base.metadata.create_all(bind=engine)
 
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://inventory-management-system-gilt-kappa.vercel.app"
-],
+        "https://inventory-management-system-gilt-kappa.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
